5b98a3bfce8944cf960045a403917814ebf027d7
Li Yong <kaven276@vip.sina.com>
2018-06-25 09:03:09 +0800

    增加 spost 范例

---
+++ pages/spost.vue
@@ -0,0 +1,27 @@
+<template>
+  <div>
+    <b-button variant="primary" @click="doPost">do post</b-button>
+    <pre v-if="ts.o">{{JSON.stringify(ts.d,null,2)}}</pre>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      ts: this.newStore(),
+    };
+  },
+  methods: {
+    doPost() {
+      this.spost(this.ts, '/ms/checkscope/', {
+        tele: '15620001781',
+      });
+    },
+  },
+};
+</script>
+
+<style>
+
+</style>
