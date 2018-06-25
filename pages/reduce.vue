3e4ce45895966a1b29c9e66033f88f8de1b46e86
Li Yong <kaven276@vip.sina.com>
2018-06-25 14:39:03 +0800

    添加 reduce 范例

---
+++ pages/reduce.vue
@@ -0,0 +1,39 @@
+<template>
+  <div>reduce demo</div>
+</template>
+
+<script>
+
+const src = [1, 4, 6, 3, 3, 7];
+const r = src.reduce(
+  (p, c, idx) => {
+    console.log(p, c, idx);
+    return p + c;
+  },
+  0,
+);
+console.log(r);
+const r2 = src.reduce(
+  (p, c, idx) => {
+    console.log(p, c, idx);
+    if (c <= 5) {
+      p.small.push(c);
+    } else {
+      p.big.push(c);
+    }
+    return p;
+  },
+  {
+    small: [],
+    big: [],
+  },
+);
+console.log(r2);
+
+export default {
+};
+</script>
+
+<style>
+
+</style>
