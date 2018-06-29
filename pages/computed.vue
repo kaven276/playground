c52e7a3b08acded9bbf399662e3fc2ad70f410b0
Li Yong <kaven276@vip.sina.com>
2018-06-29 10:15:14 +0800

    add 计算值

---
+++ pages/computed.vue
@@ -0,0 +1,57 @@
+<template>
+  <div>
+    <b-button @click="addItem">add item</b-button>
+    <div id="compare">
+      <ul>
+        <li v-for="(i) in list" :key="i.id">
+          <input type="checkbox" v-model="i.checked">
+          {{i.value}}
+        </li>
+      </ul>
+
+      <ul>
+        <li v-for="(i) in checkedList" :key="i.id">
+          <input type="checkbox" v-model="i.checked">
+          {{i.value}}
+        </li>
+      </ul>
+    </div>
+
+  </div>
+
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      list: [],
+    };
+  },
+  methods: {
+    addItem() {
+      this.list.push({
+        id: this.list.length,
+        value: Math.floor(Math.random() * 100),
+        checked: Math.random() < 0.5,
+      });
+    },
+  },
+  computed: {
+    checkedList() {
+      return this.list.filter(item => item.checked);
+    },
+  },
+};
+</script>
+
+<style lang="less">
+#compare {
+  display: flex;
+  flex-direction: row;
+  justify-content: space-around;
+  > ul {
+    width: 40%;
+  }
+}
+</style>
