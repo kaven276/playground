ef9b5719589a428cae079452d705d720177869a7
Li Yong <kaven276@vip.sina.com>
2018-07-31 15:51:21 +0800

    nest 组件范例

---
+++ components/Nested.vue
@@ -0,0 +1,29 @@
+<template>
+  <ul>
+    <template v-for="(v,idx) in list">
+      <Nested :list="v" v-if="v.length" :key="idx"/>
+      <li v-else :key="idx">{{v}}</li>
+    </template>
+  </ul>
+</template>
+
+<script>
+import Nested from '~/components/Nested.vue';
+
+export default {
+  components: {
+    Nested,
+  },
+  name: 'Nested',
+  props: {
+    list: {
+      type: Array,
+      required: true,
+    },
+  },
+};
+</script>
+
+<style>
+
+</style>
---
+++ pages/nested.vue
@@ -0,0 +1,27 @@
+<template>
+  <Nested :list="tree" />
+</template>
+
+<script>
+import Nested from '~/components/Nested.vue';
+
+export default {
+  components: {
+    Nested,
+  },
+  data() {
+    return {
+      tree: [
+        1,
+        [31, 32, 33],
+        2,
+        [41, 42, 43],
+      ],
+    };
+  },
+};
+</script>
+
+<style>
+
+</style>
