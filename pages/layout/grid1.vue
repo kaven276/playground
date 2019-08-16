baa677f38ff5f88212940d837db70f87f29c100c
Li Yong <kaven276@vip.sina.com>
2019-08-16 15:01:04 +0800

    add grid 1 2

---
+++ pages/layout/grid1.vue
@@ -0,0 +1,42 @@
+<template>
+  <div id="grid1">
+    <div v-for="i in 16" :key="i"
+      :style="makeCss()"
+    >{{i}}</div>
+  </div>
+</template>
+
+<script>
+function r(n) {
+  return Math.floor(Math.random() * n);
+}
+export default {
+  methods: {
+    makeCss() {
+      return {
+        // order: Math.floor(Math.random() * 20),
+        // gridRowStart: r(4) + 1,
+        gridColumn: `${r(2) + 1} / ${r(2) + 3}`,
+        backgroundColor: `rgb(${r(256)}, ${r(256)}, ${r(256)})`,
+      };
+    },
+  },
+};
+</script>
+
+<style scoped lang="less">
+#grid1 {
+  display: grid;
+  grid-template-rows: repeat(5, minmax(120px, auto));
+  grid-template-columns: repeat(5, 1fr);
+  grid-row-gap: 10px;
+  grid-column-gap: 10px;
+  justify-items: stretch;
+  align-items: stretch;
+  outline: 1px solid rgb(23, 21, 21);
+
+  > div {
+   // border: 1px solid red;
+  }
+}
+</style>
---
+++ pages/layout/grid2.vue
@@ -0,0 +1,45 @@
+<template>
+  <div id="grid2">
+    <div></div>
+  </div>
+</template>
+
+<script>
+function r(n) {
+  return Math.floor(Math.random() * n);
+}
+export default {
+  methods: {
+    makeCss() {
+      return {
+        // order: Math.floor(Math.random() * 20),
+        // gridRowStart: r(4) + 1,
+        gridColumn: `${r(2) + 1} / ${r(2) + 3}`,
+        backgroundColor: `rgb(${r(256)}, ${r(256)}, ${r(256)})`,
+      };
+    },
+  },
+};
+</script>
+
+<style scoped lang="less">
+#grid2 {
+  padding: 10px;
+  display: grid;
+  grid-template-rows: repeat(5, minmax(120px, auto));
+  grid-template-columns: repeat(5, 1fr);
+  grid-row-gap: 10px;
+  grid-column-gap: 10px;
+  justify-items: stretch;
+  align-items: stretch;
+  outline: 1px solid rgb(23, 21, 21);
+
+  > div {
+   border: 1px solid red;
+   grid-column-start: 3;
+   grid-column-end: 6;
+   grid-row-start: 3;
+   grid-row-end: 5;
+  }
+}
+</style>
